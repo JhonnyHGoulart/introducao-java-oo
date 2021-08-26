1da8e881683105bbee923080dbb60812871ad7d7
Jhonny Hilário Goulart <jhonny.goulart@sc.senai.br>
2021-08-26 13:37:02 -0300

    Duas Variáveis de referência referenciando o mesmo objeto

---
+++ src/br/com/senai/introducao/ReferenciaTest.java
@@ -0,0 +1,20 @@
+package br.com.senai.introducao;
+
+public class ReferenciaTest {
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta();
+        primeiraConta.setSaldo(300);
+        Conta segundaConta = primeiraConta;
+        segundaConta.setSaldo(segundaConta.getSaldo() + 100);
+        System.out.println("Saldo da primeira conta: " + primeiraConta.getSaldo());
+        System.out.println(segundaConta.getSaldo());
+
+        boolean tipo;
+        if (primeiraConta == segundaConta) {
+            tipo = true;
+        } else {
+            tipo = false;
+        }
+        System.out.println("É a mesma conta? " + tipo);
+    }
+}
