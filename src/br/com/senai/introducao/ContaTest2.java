104adbaeb9a8e994efaaf0beace05412193f1008
Jhonny Hilário Goulart <jhonny.goulart@sc.senai.br>
2021-08-26 13:08:04 -0300

    Valores default na criação de objetos

---
+++ src/br/com/senai/introducao/ContaTest2.java
@@ -0,0 +1,12 @@
+package br.com.senai.introducao;
+
+public class ContaTest2 {
+    public static void main(String[] args) {
+        Conta conta2 = new Conta();
+
+        System.out.println(conta2.getSaldo());
+        System.out.println(conta2.getAgencia());
+        System.out.println(conta2.getNumero());
+        System.out.println(conta2.getTitular());
+    }
+}
